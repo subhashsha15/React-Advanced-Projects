bea192e85877a0c4052078998309b3dd5913eb19
subhashsha15 <subhashsha15@gmail.com>
2023-11-03 20:11:51 +0530

    code

--- 3D Portfolio/src/components/Hero.jsx
+++ 3D Portfolio/src/components/Hero.jsx
@@ -20,8 +20,8 @@ const Hero = () => {
       </div>
       <ComputersCanvas />
       <div className='absolute xs:bottom-10 mb-[-35px] bottom-0 w-full flex justify-center items-center'>
-        <a href="#about" className='hover:text-purple-500'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+        <a href="#about" >
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 hover:border-purple-500'>
           <motion.div
               animate={{
                 y: [0, 24, 0],
@@ -31,7 +31,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1 hover:bg-sky-700'
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
             />
           </div>
         </a>
